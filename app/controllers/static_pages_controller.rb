5d41bbdd509496f61c88e57d8b951b67f522a859
Mitchell Gu <mx.mitchell.gu@gmail.com>
2014-10-04 16:37:53 -0400

    gets longitude/latitude at location page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,17 @@
 class StaticPagesController < ApplicationController
+	require 'net/http'
+	require 'json'
   def homepage
+  end
+  def location
+  	if params['building']
+  		url = URI.parse('http://m.mit.edu/apis/maps/places/?q=' + params['building'])
+			req = Net::HTTP::Get.new(url.to_s)
+			res = Net::HTTP.start(url.host, url.port) {|http|
+			  http.request(req)
+			}
+			content_hash = JSON.parse(res.body)
+			@content = "Coordinates of " + params['building'].to_s + ": " + [content_hash[0]['lat_wgs84'],content_hash[0]['long_wgs84']].to_s
+  	end
   end
 end
