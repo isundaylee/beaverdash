b078512e87cbf5b68959db210688e05edf1e40c6
Jiahao Li <isundaylee.reg@gmail.com>
2014-10-05 20:35:42 -0400

    Comments out default template.

--- config/initializers/app_config_sensitive.template.rb
+++ config/initializers/app_config_sensitive.template.rb
@@ -1,10 +1,10 @@
-APP_CONFIG ||= {}
-APP_CONFIG = APP_CONFIG.deep_merge({
-  yo: {
-    api_key: 'YO_API_KEY'
-  },
-  fitbit: {
-    key: 'FITBIT_KEY',
-    secret: 'FITBIT_SECRET'
-  }
-})
+# APP_CONFIG ||= {}
+# APP_CONFIG = APP_CONFIG.deep_merge({
+#   yo: {
+#     api_key: 'YO_API_KEY'
+#   },
+#   fitbit: {
+#     key: 'FITBIT_KEY',
+#     secret: 'FITBIT_SECRET'
+#   }
+# })
