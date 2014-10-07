13cbeda38cf000d1b858b18bf7a75332a228ceec
Jiahao Li <isundaylee.reg@gmail.com>
2014-10-07 11:59:07 -0400

    Fixes FITBIT.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,24 +10,16 @@ class StaticPagesController < ApplicationController
     @weight = DEFAULT_WEIGHT
 
     if session[:fitbit_token].nil?
-      client = Fitbit::Client.new({:consumer_key => APP_CONFIG[:fitbit][:key], :consumer_secret => APP_CONFIG[:fitbit][:secret]})
-      request = client.request_token
-      session[:request_secret] = request.secret
-      @fitbit_link = "http://www.fitbit.com/oauth/authorize?oauth_token=#{request.token}"
+      # client = Fitbit::Client.new({:consumer_key => APP_CONFIG[:fitbit][:key], :consumer_secret => APP_CONFIG[:fitbit][:secret]})
+      # request = client.request_token
+      # session[:request_secret] = request.secret
+      # @fitbit_link = "http://www.fitbit.com/oauth/authorize?oauth_token=#{request.token}"
+      @fitbit_link = "javascript: void(0); "
     else
-      client = Fitbit::Client.new({:consumer_key => APP_CONFIG[:fitbit][:key], :consumer_secret => APP_CONFIG[:fitbit][:secret], :token => session[:fitbit_token], :secret => session[:fitbit_secret]})
-      access_token = client.reconnect(session[:fitbit_token], session[:fitbit_secret])
-      @weight = client.user_info['user']['weight'].to_f rescue DEFAULT_WEIGHT
-
-      # puts client.log_activity({
-      #   activityName: 'biking',
-      #   durationMillis: 360000,
-      #   startTime: DateTime.now.strftime('%H:%M'),
-      #   date: DateTime.now.strftime('%Y-%m-%d'),
-      #   distance: 1.37,
-      #   manualCalories: 123,
-      #   distanceUnit: Fitbit::ApiUnitSystem.METRIC
-      # })
+      # client = Fitbit::Client.new({:consumer_key => APP_CONFIG[:fitbit][:key], :consumer_secret => APP_CONFIG[:fitbit][:secret], :token => session[:fitbit_token], :secret => session[:fitbit_secret]})
+      # access_token = client.reconnect(session[:fitbit_token], session[:fitbit_secret])
+      # @weight = client.user_info['user']['weight'].to_f rescue DEFAULT_WEIGHT
+      @weight = DEFAULT_WEIGHT
     end
   end
 end
